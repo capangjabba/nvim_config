543dba48abcacebba6ad3146a083e61f22287194
capangjabba <ihaziq18.ih@gmail.com>
2025-11-18 02:33:58 +0800

    configure python lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,1 +1,9 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+require('mason').setup()
+local mason_lspconfig = require 'mason-lspconfig'
+mason_lspconfig.setup {
+ensure_installed = { "pyright" }
+}
+require("lspconfig").pyright.setup {}
+
 require("lspconfig").lua_ls.setup {}
